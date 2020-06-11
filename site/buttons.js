08620c4a5726a017856bfe43a40dbfac394daca3
caleymoon <caleymoon@lclark.edu>
2020-06-11 08:50:16 -0700

    Caley's changes here

--- site/buttons.js
+++ site/buttons.js
@@ -1,15 +1,78 @@
-$('.simon-button.green').on('click', function() {
-  alert('You clicked the green button');
-});
 
-$('.simon-button.blue').on('click', function() {
-  alert('You clicked the blue button');
-});
+//Green
+  $('.simon-button.green').on('mouseup', function() {
+    $('.simon-button.green').removeClass('light-up');
+    let text = $('.counter').text()
+    console.log(text)
+    text = parseInt(text)
+    console.log(text)
+    $('.counter').text(text+1)
+  });
+  
+  $('.simon-button.green').on('mousedown', function() {
+    $('.simon-button.green').addClass('light-up');
+    console.log("GREEN");
+  });
 
-$('.simon-button.yellow').on('click', function() {
-  alert('You clicked the yellow button');
-});
+  //Red
+  $('.simon-button.red').on('mouseup', function() {
+    $('.simon-button.red').removeClass('light-up')
+    let text = $('.counter').text()
+    console.log(text)
+    text = parseInt(text)
+    console.log(text)
+    $('.counter').text(text+1);
+  });
+  
+  $('.simon-button.red').on('mousedown', function() {
+    $('.simon-button.red').addClass('light-up');
+    console.log("RED");
+  });
 
-$('.simon-button.red').on('click', function() {
-  alert('You clicked the red button');
-});
+  //Yellow
+  $('.simon-button.yellow').on('mouseup', function() {
+    $('.simon-button.yellow').removeClass('light-up');
+    let text = $('.counter').text()
+    console.log(text)
+    text = parseInt(text)
+    console.log(text)
+    $('.counter').text(text+1)
+  });
+  $('.simon-button.yellow').on('click', function(){
+    var audio = new Audio("terra.m4a");
+    audio.play();
+  });
+  
+  $('.simon-button.yellow').on('mousedown', function() {
+    $('.simon-button.yellow').addClass('light-up');
+    console.log("YELLOW");
+  });
+
+  //Blue
+  $('.simon-button.blue').on('mouseup', function() {
+    $('.simon-button.blue').removeClass('light-up');
+    let text = $('.counter').text()
+    console.log(text)
+    text = parseInt(text)
+    console.log(text)
+    $('.counter').text(text+1)
+  });
+  
+  $('.simon-button.blue').on('mousedown', function() {
+    $('.simon-button.blue').addClass('light-up');
+    console.log("BLUE");
+  });
+
+  $('.simon-button.green').on('click', function(){
+    var audio = new Audio("malena.m4a");
+    audio.play();
+  });  
+  $('.simon-button.red').on('click', function(){
+    var audio = new Audio("jesse.m4a");
+    audio.play();
+  });  
+  $('.simon-button.blue').on('click', function(){
+    var audio = new Audio("isaac.m4a");
+    audio.play();
+  });  
+  
